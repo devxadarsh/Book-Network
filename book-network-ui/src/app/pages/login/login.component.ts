3063ca1738b8896fc674dca546f309465d16b178
devxadarsh <devxadarsh@gmail.com>
2024-08-09 23:00:50 +0530

    .

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthenticationRequest } from '../../services/models';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../services/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +14,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginComponent {
 
-  authRequest : AuthenticationRequest = { email: '', password: ''}
-  errorMsg : Array<string> = [];
+  authRequest: AuthenticationRequest = { email: '', password: '' }
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) { }
 
   login() {
 
   }
 
   register() {
-    
+    this.router.navigate(['/register'])
   }
 
 }

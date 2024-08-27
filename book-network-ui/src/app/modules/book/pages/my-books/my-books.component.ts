a2292a718352ef79b678eb3e65a24d38f96e7d00
devxadarsh <devxadarsh@gmail.com>
2024-08-27 22:50:48 +0530

    added manage-book

--- book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
+++ book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { PageResponseBookResponse } from '../../../../services/models/page-response-book-response';
 import { BookService } from '../../../../services/services/book.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from '../../components/book-card/book-card.component';
+import { BookResponse } from '../../../../services/models';
 
 @Component({
   selector: 'app-my-books',
   standalone: true,
-  imports: [CommonModule, BookCardComponent],
+  imports: [CommonModule, BookCardComponent, RouterLink],
   templateUrl: './my-books.component.html',
   styleUrl: './my-books.component.scss',
 })
@@ -28,7 +29,7 @@ export class MyBooksComponent {
 
   findAllBooks() {
     this.bookService
-      .findAllBooks({
+      .findAllBooksByOwner({
         page: this.page,
         size: this.size,
       })
@@ -67,5 +68,33 @@ export class MyBooksComponent {
 
   get isLastPage(): boolean {
     return this.page == (this.bookResponse.totalPage as number) - 1;
+  }
+
+  archiveBook(book: BookResponse) {
+    this.bookService
+      .updateArchivedStatus({
+        'book-id': book.id as number,
+      })
+      .subscribe({
+        next: () => {
+          book.archived = !book.archived;
+        },
+      });
+  }
+
+  shareBook(book: BookResponse) {
+    this.bookService
+      .updateSharableStatus({
+        'book-id': book.id as number,
+      })
+      .subscribe({
+        next: () => {
+          book.shareable = !book.shareable;
+        },
+      });
+  }
+
+  editBook(book: BookResponse) {
+    this.router.navigate(['books', 'manage', book.id]);
   }
 }

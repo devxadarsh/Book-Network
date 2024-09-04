4247a2a93cce2a8f2fb343fdb76f587954573f3e
devxadarsh <devxadarsh@gmail.com>
2024-09-04 22:37:39 +0530

    .

--- book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,5 +1,5 @@
 import { BookService } from './../../../../services/services/book.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BookRequest } from '../../../../services/models';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-manage-book',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, JsonPipe],
   templateUrl: './manage-book.component.html',
   styleUrl: './manage-book.component.scss',
 })
@@ -49,6 +49,7 @@ export class ManageBookComponent implements OnInit {
             if (book.cover) {
               this.selectedPicture = 'data:image/jpg;base64,' + book.cover;
             }
+            console.log('this is book data', book);
           },
         });
     }

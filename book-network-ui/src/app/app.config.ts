ec29b8f407e89fb2032b51ddc0a4c358dcf249bf
devxadarsh <devxadarsh@gmail.com>
2024-09-15 18:46:32 +0530

    pipeline fronted updated

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -1,25 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { httpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { ApiModule } from './services/api.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    importProvidersFrom(
+      ApiModule.forRoot({ rootUrl: 'http://192.168.0.227:8088/api/v1' })
+    ),
     // provideHttpClient(),
     // {
     //   provide: HTTP_INTERCEPTORS,
     //   useClass: HttpTokenInterceptor,
     //   multi: true
     // }
-    provideHttpClient(withInterceptors(
-      [
-        httpTokenInterceptor
-      ]
-    ))
+    provideHttpClient(withInterceptors([httpTokenInterceptor])),
   ],
 };

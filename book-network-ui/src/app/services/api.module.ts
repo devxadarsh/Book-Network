678b1b43d2fe383fbd0209b9cec5bfa3451d304e
devxadarsh <devxadarsh@gmail.com>
2024-09-16 09:29:12 +0530

    .

--- book-network-ui/src/app/services/api.module.ts
+++ book-network-ui/src/app/services/api.module.ts
@@ -1,6 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
-import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import {
+  NgModule,
+  ModuleWithProviders,
+  SkipSelf,
+  Optional,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiConfiguration, ApiConfigurationParams } from './api-configuration';
 
@@ -9,7 +14,7 @@ import { BookService } from './services/book.service';
 import { AuthenticationService } from './services/authentication.service';
 
 /**
- * Module that provides all services and configuration.
+ * Module that provides all services and configurations.
  */
 @NgModule({
   imports: [],
@@ -19,32 +24,38 @@ import { AuthenticationService } from './services/authentication.service';
     FeedbackService,
     BookService,
     AuthenticationService,
-    ApiConfiguration
+    ApiConfiguration,
   ],
 })
 export class ApiModule {
-  static forRoot(params: ApiConfigurationParams): ModuleWithProviders<ApiModule> {
+  static forRoot(
+    params: ApiConfigurationParams
+  ): ModuleWithProviders<ApiModule> {
     return {
       ngModule: ApiModule,
       providers: [
         {
           provide: ApiConfiguration,
-          useValue: params
-        }
-      ]
-    }
+          useValue: params,
+        },
+      ],
+    };
   }
 
-  constructor( 
+  constructor(
     @Optional() @SkipSelf() parentModule: ApiModule,
     @Optional() http: HttpClient
   ) {
     if (parentModule) {
-      throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+      throw new Error(
+        'ApiModule is already loaded. Import in your base AppModule only.'
+      );
     }
     if (!http) {
-      throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
-      'See also https://github.com/angular/angular/issues/20575');
+      throw new Error(
+        'You need to import the HttpClientModule in your AppModule! \n' +
+          'See also https://github.com/angular/angular/issues/20575'
+      );
     }
   }
 }
